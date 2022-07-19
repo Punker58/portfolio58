32ba899ad317822f5d4ff7ecc8297475b539fce3
Punker58 <mistershowoff@gmail.com>
2022-07-19 18:00:09 +0200

    0.0.8 - aggiunte icone sezione social

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,11 +1,16 @@
+import igpic from '../img/icon/ig.png';
+import fbpic from '../img/icon/fb.png';
+import rdpic from '../img/icon/reddit.png';
+import ghpic from '../img/icon/github.png';
+
 function  Social() {
     return(
         <>
             <div className="social float-end">
-                <a>INSTA</a>
-                <a>FB</a>
-                <a>REDDIT</a>
-                <a>GITHUB</a>
+                <a><img className="img-fluid" src={igpic} alt="instagram"/></a>
+                <a><img className="img-fluid" src={fbpic} alt="facebook"/></a>
+                <a><img className="img-fluid" src={rdpic} alt="reddit"/></a>
+                <a><img className="img-fluid" src={ghpic} alt="github"/></a>
             </div>
         </>
     );
